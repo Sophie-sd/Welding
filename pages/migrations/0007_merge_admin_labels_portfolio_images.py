0c4a03fc45c8f6fec229bcb1661661668cf6a572
Sophie-sd <sofiia.dmitrenko@gmail.com>
2026-07-02 12:52:42 +0300

    Fix conflicting pages migrations with merge 0007

---
+++ pages/migrations/0007_merge_admin_labels_portfolio_images.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pages', '0006_admin_uk_labels'),
+        ('pages', '0006_update_portfolio_images'),
+    ]
+
+    operations = []
